323a4bc6ca9acb814769ec333a15ed1fe62e600f
chasomin <somin49@naver.com>
2024-01-13 21:37:45 +0900

    트레블톡 화면 구현(ChatList)

--- TravelTalk/ChatList/ChatListTableViewCell.swift
+++ TravelTalk/ChatList/ChatListTableViewCell.swift
@@ -8,16 +8,57 @@
 import UIKit
 
 class ChatListTableViewCell: UITableViewCell {
-
+    static let id = "ChatListTableViewCell"
+    
+    @IBOutlet var chatroomImageView: UIImageView!
+    @IBOutlet var chatroomNameLabel: UILabel!
+    @IBOutlet var chatMessageLabel: UILabel!
+    @IBOutlet var chatDateLabel: UILabel!
+    
+    
+    let format = DateFormatter()
+    let formatString = DateFormatter()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
+        
+        DispatchQueue.main.async {
+            self.chatroomImageView.layer.cornerRadius = self.chatroomImageView.frame.width / 2
+        }
+        chatroomImageView.contentMode = .scaleAspectFill
+        
+        chatroomNameLabel.font = .boldSystemFont(ofSize: 16)
+        
+        chatMessageLabel.font = .systemFont(ofSize: 14)
+        chatMessageLabel.textColor = .gray
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        chatDateLabel.font = .systemFont(ofSize: 12)
+        chatDateLabel.textColor = .gray
+        
+        format.dateFormat = "yyyy-MM-dd HH:mm"
+        formatString.dateFormat = "yy.MM.dd"
+        
+        selectionStyle = .none
 
-        // Configure the view for the selected state
     }
     
+
+    func setCell(data: ChatRoom) {
+        chatroomImageView.image = UIImage(named: data.chatroomImage[0])
+        
+        chatroomNameLabel.text = data.chatroomName
+        
+        chatMessageLabel.text = data.chatList.last?.message
+        
+        guard let date = format.date(from: data.chatList.last?.date ?? "") else {
+            return
+        }
+        
+        let result = formatString.string(from: date)
+
+        chatDateLabel.text = result
+        
+    }
+    
+
 }
--- TravelTalk/ChatList/ChatListViewController.swift
+++ TravelTalk/ChatList/ChatListViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let xib = UINib(nibName: ChatListTableViewCell.id, bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: ChatListTableViewCell.id)
 
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        navigationItem.title = "TRAVEL TALK"
+        
+        tableView.rowHeight = 85
+        tableView.separatorStyle = .none
+        
+        searchBar.placeholder = "친구 이름을 검색해보세요"
     }
+    
+    
     
 
 
+}
+
+
+
+extension ChatListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        mockChatList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ChatListTableViewCell.id, for: indexPath) as! ChatListTableViewCell
+        
+        cell.setCell(data: mockChatList[indexPath.row])
+        return cell
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
 }

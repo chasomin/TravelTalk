2d483758b0f9c6cb013e79005c56a402bd0b641f
chasomin <somin49@naver.com>
2024-01-13 21:35:01 +0900

    트레블톡 화면 구현 (ChatList)

---
+++ TravelTalk/ChatList/ChatListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatListViewController.swift
+//  TravelTalk
+//
+//  Created by 차소민 on 1/13/24.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}

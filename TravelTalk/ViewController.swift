b22b1ab2ce08151a0fbb567588d26d035afb2ab8
chasomin <somin49@naver.com>
2024-01-13 19:59:58 +0900

    Initial Commit

---
+++ TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by 차소민 on 1/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
